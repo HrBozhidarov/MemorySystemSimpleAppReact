73a791dce6d4df7c824ba46b88ea8a951785c2f3
h.a.bozhidarov@gmail.com <h.a.bozhidarov@gmail.com>
2022-06-07 19:40:50 +0300

    Remove no need project and add .gitignore file.

--- MemorySystemClient/src/constants/constants.js
+++
@@ -1,8 +0,0 @@
-export const DOMAIN_URL = 'https://localhost:59512/'
-
-export const ACCOUNT_KEYS = {
-    ROLE: 'role',
-    MEMORY_CATEGORY: 'my-memory-category-key',
-    TOKEN: 'token',
-    USER_PROFILE_PICTURE: 'user-profile-picture',
-}
