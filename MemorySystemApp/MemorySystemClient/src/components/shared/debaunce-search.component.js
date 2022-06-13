620cb58abddc307ad403cef916e0dbfcf6c5d43d
hristo.bozhidarov <hristo.bozhidarov@smartit.bg>
2022-06-13 16:18:43 +0300

    Add DebaunceSearch component.

---
+++ MemorySystemApp/MemorySystemClient/src/components/shared/debaunce-search.component.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useMemo } from 'react';
+import debounce from 'lodash.debounce';
+
+function DebaunceSearch({searchHandler}) {
+    const debouncedChangeHandler = useMemo(() => debounce(searchHandler, 300), []);
+
+        useEffect(() => {
+            return () => {
+              debouncedChangeHandler.cancel();
+            }
+          }, []);
+
+    return (
+        <>
+            <div className="form-group has-search">
+                <span className="fa fa-search form-control-feedback"></span>
+                <input onChange={debouncedChangeHandler} type="text" className="form-control" placeholder="Search" />
+            </div>
+        </>
+    )
+}
+
+export default DebaunceSearch;
